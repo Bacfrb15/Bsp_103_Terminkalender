9530d45012c7d90d3cc6f5be5b0d8210f2c61429
Fink Patrick <finpac14@htlkaindorf.at>
2018-10-01 11:24:22 +0200

    IMplementierung der Datenerhaltungsklasse

--- Bsp_103_Terminkalender/src/ex003/Appointment.java
+++ Bsp_103_Terminkalender/src/ex003/Appointment.java
@@ -1,9 +1,27 @@
 package ex003;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 /**
  *
  * @author franz
  */
 public class Appointment {
+    private LocalDateTime ldt;
+    private String terminname;
+    private DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd.MM.yyyy - HH:mm");
+
+    public Appointment(LocalDateTime ldt, String terminname) {
+        this.ldt = ldt;
+        this.terminname = terminname;
+    }
+
+    
+    @Override
+    public String toString() {
+        return ldt.format(dtf) + " --> " + terminname;
+    }
+    
     
 }
